f88953a6bc460749ce168e29a163ad9df323e3ba
Gui-dev <adrianon10@gmail.com>
2022-10-05 03:04:24 -0300

    feat: add jump 2

--- scripts/player/character_base.gd
+++ scripts/player/character_base.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 class_name CharacterBase
 
 
-enum STATE_MACHINE {IDLE, WALK, JUMP}
+enum STATE_MACHINE {IDLE, WALK, JUMP, DOUBLE_JUMP, FALL}
 var state = STATE_MACHINE.IDLE
 var motion := Vector2()
 var animation_name := ''
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -6,6 +6,8 @@ func _process(delta: float) -> void:
     STATE_MACHINE.IDLE: _state_idle(delta)
     STATE_MACHINE.WALK: _state_walk(delta)
     STATE_MACHINE.JUMP: _state_jump(delta)
+    STATE_MACHINE.DOUBLE_JUMP: _state_double_jump(delta)
+    STATE_MACHINE.FALL: _state_fall(delta)
 
 
 func _state_idle(delta: float) -> void:
@@ -30,16 +32,43 @@ func _state_walk(delta: float) -> void:
     _enter_state(STATE_MACHINE.IDLE)
   
   if Input.is_action_just_pressed('ui_jump'):
-    _enter_state(STATE_MACHINE.JUMP)
+    _enter_state(STATE_MACHINE.DOUBLE_JUMP)
 
 
 func _state_jump(delta: float) -> void:
   _set_animation('jump')
   _apply_gravity(delta)
+  _set_flip()
+  _move_and_slide(delta)
   
   if enter_state:
     enter_state = false
     motion.y = -jump_force
     
   if motion.y > 0:
+    _enter_state(STATE_MACHINE.FALL)
+
+
+func _state_double_jump(delta: float) -> void:
+  _set_animation('jump_2')
+  _apply_gravity(delta)
+  _set_flip()
+  _move_and_slide(delta)
+  
+  if enter_state:
+    enter_state = false
+    motion.y = -jump_force
+  
+  if motion.y > 0:
+    _enter_state(STATE_MACHINE.FALL)
+    
+
+
+func _state_fall(delta: float) -> void:
+  _set_animation('fall')
+  _apply_gravity(delta)
+  _set_flip()
+  _move_and_slide(delta)
+  
+  if is_on_floor():
     _enter_state(STATE_MACHINE.IDLE)
